ccf7f82595582a3e5e87de9ec1d0cce27f9e67c2
peyras <cecilelibata@gmail.com>
2019-05-12 10:16:42 +0200

    une dernière moidif

--- src/test/java/fr/pizzeria/service/AjouterPizzaServiceTest.java
+++ src/test/java/fr/pizzeria/service/AjouterPizzaServiceTest.java
@@ -12,41 +12,32 @@ import fr.pizzeria.exception.SavePizzaException;
 import fr.pizzeria.model.Pizza;
 import fr.pizzeria.model.PizzaMemDao;
 
-
 public class AjouterPizzaServiceTest {
-	
+
 	@Rule
 	public TextFromStandardInputStream systemInMock = TextFromStandardInputStream.emptyStandardInputStream();
 
 	@Test
-	public void testExecuteUC() {
+	public void testExecuteUC() throws SavePizzaException {
 		systemInMock.provideLines("MAX", "pizzaTest", "15", "VIANDE");
-		
-		
-		PizzaMemDao dao = new PizzaMemDao(); 
-		AjouterPizzaService ajout = new AjouterPizzaService(); 
-		try {
-			ajout.executeUC(new Scanner(System.in), dao);
-		} catch (SavePizzaException e) {
-			e.printStackTrace();
-		}
-	
-		assertEquals(9, dao.findAllPizzas().size() );
-		
-	}
-	
-	@Test (expected = SavePizzaException.class)
-	public void testExecuteUC2() throws SavePizzaException {
-		systemInMock.provideLines("", "pizzaTest", "15", "VIANDE");
-		
-		
-		PizzaMemDao dao = new PizzaMemDao(); 
-		AjouterPizzaService ajout = new AjouterPizzaService(); 
-		
-			ajout.executeUC(new Scanner(System.in), dao);
-	
-		
+
+		PizzaMemDao dao = new PizzaMemDao();
+		AjouterPizzaService ajout = new AjouterPizzaService();
+		ajout.executeUC(new Scanner(System.in), dao);
+
+		assertEquals(9, dao.findAllPizzas().size());
+
 	}
 
+	@Test(expected = SavePizzaException.class)
+	public void testExecuteUC2() throws SavePizzaException {
+		systemInMock.provideLines("", "pizzaTest", "15", "VIANDE");
+
+		PizzaMemDao dao = new PizzaMemDao();
+		AjouterPizzaService ajout = new AjouterPizzaService();
+
+		ajout.executeUC(new Scanner(System.in), dao);
+
+	}
 
 }
